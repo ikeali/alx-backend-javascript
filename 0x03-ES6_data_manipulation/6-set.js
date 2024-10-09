0b6c1456cc51629e9655fbc3d0b02ff632cf6db2
ikeali <ikechukwualisigwe01@gmail.com>
2024-10-09 10:57:17 +0000

    6-set.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Ikechukwu Alisigwe <https://github.com/ikeali>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
