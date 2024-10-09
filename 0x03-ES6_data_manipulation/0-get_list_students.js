b7ddc860d0fe51d205e9d3ae31b0c9540f5462fc
ikeali <ikechukwualisigwe01@gmail.com>
2024-10-09 10:41:33 +0000

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,12 @@
+/**
+ * Retrieves a list of students.
+ * @author Ikechukwu Alisigwe <https://github.com/ikeali>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
-
